b7e2f0361fdc4b2ab0f28d6589fa318c3a1e04aa
ohtaman <ohtamans@gmail.com>
2017-01-11 22:26:42 +0900

    [modify] Modify annealer

--- anneal/annealers/annealer.py
+++ anneal/annealers/annealer.py
@@ -1,28 +1,38 @@
 # -*- coding:utf-8 -*-
 
 import abc
-
+import math
 
 class Annealer(metaclass=abc.ABCMeta):
     def initialize(self, initial_state):
-        return initial_state
-
-    def on_accept(self, candidate_state, candidate_energy):
-        pass
-
-    def on_reject(self, candidate_state, candidate_energy):
-        pass
+        self.current_state = initial_state
+        self.current_energy = self.energy(initial_state)
+        self.step_count = 0
 
     @abc.abstractmethod
-    def energy(self, state):
+    def set_state(self, state, energy=None):
+        if energy is None:
+            energy = self.get_energy(state)
+        self.current_state = state
+        self.current_energy = energy
+ 
+    @abc.abstractmethod
+    def get_energy(self, state):
         pass
 
     @abc.abstractmethod
     def get_neighbor(self, state):
         pass
 
+    def count_up(self):
+        self.step_count += 1
+
+    def is_acceptable(self, candidate_energy):
+        energy_diff = min(0.0, candidate_energy - self.current_energy)
+        return math.exp(-energy_diff/self.get_temperarute())
+
     @abc.abstractmethod
-    def is_acceptable(self, candidate_state, candidate_energy):
+    def get_temperature(self):
         pass
 
     @abc.abstractmethod
@@ -33,17 +43,13 @@ class Annealer(metaclass=abc.ABCMeta):
         return self.optimize(**kwargs)
 
     def optimize(self, initial_state=None):
-        self.current_state = self.initialize(initial_state)
-        self.current_energy = self.energy(self.current_state)
-        self.iter_step = 0
+        self.initialize(initial_state)
 
         while not self.is_frozen():
             candidate_state = self.get_neighbor(self.current_state)
-            candidate_energy = self.energy(candidate_state)
-            if self.is_acceptable(candidate_state, candidate_energy):
-                self.current_state = candidate_state
-                self.current_energy = candidate_energy
-
-            self.iter_step += 1
+            candidate_energy = self.get_energy(candidate_state)
+            if self.is_acceptable(candidate_energy):
+                self.set_state(candidate_state, candidate_energy)
+            self.count_up()
 
         return self.state
