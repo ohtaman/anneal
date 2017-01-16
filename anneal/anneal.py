cea0ea9427a6191b8fd2b8c194cfbb0c80fc8bb9
ohtaman <ohtamans@gmail.com>
2017-01-16 23:25:55 +0900

    [modify] Add kwargs for get_energy method

--- anneal/anneal.py
+++ anneal/anneal.py
@@ -17,17 +17,10 @@ class PhysicalModel(metaclass=abc.ABCMeta):
     def get_state(self):
         pass
 
-    @property
-    def state(self):
-        return self.get_state()
-
     @abc.abstractmethod
-    def get_energy(self):
+    def get_energy(self, **kwargs):
         pass
 
-    @property
-    def energy(self):
-        return self.get_energy()
 
     @abc.abstractmethod
     def update(self, **kwargs):
@@ -62,15 +55,15 @@ class ClassicalIsingModel(PhysicalModel):
             assert h.shape == lattice_shape
             self._h = h.flatten()
 
-        self._state = self._generate_initial_state()
+        self._state = self._generate_initial_state(lattice_shape)
 
-    def _generate_initial_state(self):
-        return np.random.randint(2, size=self.lattice_shape)*2 - 1
+    def _generate_initial_state(self, lattice_shape):
+        return np.random.randint(2, size=lattice_shape)*2 - 1
 
     def get_state(self):
         return self._state
 
-    def get_energy(self):
+    def get_energy(self, **kwargs):
         flatten = self._state.flatten()
         e = flatten.T.dot(self._j.dot(flatten))
         e += self._h.dot(flatten) if self.h else 0.
@@ -101,6 +94,91 @@ class ClassicalIsingModel(PhysicalModel):
             value = self.state.take(idx)
             self.state.put(idx, -value)
         return self.state
+
+
+class QuantumIsingModel(PhysicalModel):
+    def __init__(self, lattice_shape, j, h=None, trotter_size=16, neighbor_size=2):
+        """Class of Ising model.
+
+        Arguments:
+            lattice_shape (tuple): Shape of lattice.
+            j (dict): Interaction strength.
+            h (np.ndarray): external field strength.
+            trotter_size (int): number of Trotter slice.
+            neighbor_size (int): Size of neighbors.
+        """
+        super().__init__()
+
+        self.lattice_shape = lattice_shape
+        self.lattice_size = np.prod(lattice_shape)
+        self.j = j
+        self.h = h
+        self.trotter_size = trotter_size
+        self.neighbor_size = neighbor_size
+
+        self._j = sp.dok_matrix((self.lattice_size, self.lattice_size))
+        for k, v in j.items():
+            row = np.ravel_multi_index(k[len(k)//2:], lattice_shape)
+            col = np.ravel_multi_index(k[:len(k)//2], lattice_shape)
+            self._j[row, col] = self._j[col, row] = v  # j must be symmetric.
+        self._h = np.zeros(self.lattice_size)
+        if h:
+            assert h.shape == lattice_shape
+            self._h = h.flatten()
+
+        self._state = self._generate_initial_state(
+            self.lattice_shape,
+            self.trotter_size
+        )
+
+    def _generate_initial_state(self, lattice_shape, trotter_size):
+        shape = self.lattice_shape + (trotter_size,)
+        return np.random.randint(2, shape)*2 - 1
+
+    def get_state(self):
+        return self._state
+
+    def get_energy(self, beta, gamma):
+        # Classical term
+        # Flatten except trotter dimension.
+        flatten = self._state.flatten()[:, self.trotter_size]
+        e = flatten.T.dot(self._j.dot(flatten)).mean()
+        e += self._h.dot(flatten).mean() if self.h else 0.
+
+        # Quantum term
+        coeff = (1/(2*beta))*self._logcoth(beta*gamma/self.trotter_size)
+
+        return e
+
+    def update(self, beta, gamma):
+        current_energy = self.get_energy(beta, gamma)
+        num_flip = np.random.randint(self.neighbor_size) + 1
+        indices = np.random.choice(
+            range(self.lattice_size),
+            num_flip,
+            replace=False
+        )
+
+        self._flip_spin(indices)
+
+        candidate_energy = self.get_energy(beta, gamma)
+        delta = max(0.0, candidate_energy - current_energy)
+
+        if math.exp(-beta*delta) >= random.random():
+            return True
+        else:
+            self._flip_spin(indices)
+            return False
+
+    def _flip_spin(self, flatten_indices):
+        for idx in flatten_indices:
+            value = self.state.take(idx)
+            self.state.put(idx, -value)
+        return self.state
+
+    @staticmethod
+    def _logcoth(x):
+        return math.log(math.cosh(x)/math.sinh(x))
 
 
 class Annealer(metaclass=abc.ABCMeta):
